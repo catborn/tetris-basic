8fc29403ca0a138be2f0a45b24743a5f3669f878
catborn <itnoz@yandex.ru>
2024-08-26 23:44:03 +0300

    add js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tetris
+
 # [Code Tetris: JavaScript Tutorial for Beginners](https://www.youtube.com/watch?v=rAUn1Lom6dw)
+
+[tetris-basic](https://github.com/kubowania/Tetris-Basic)
--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelector(".grid div"));
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
+  const width = 10;
+  console.log(squares[0]);
+});
