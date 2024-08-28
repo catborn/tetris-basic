bf5adfe83acfac9784cd8edcad9625ee0ca2cbe6
catborn <158094798+catborn@users.noreply.github.com>
2024-08-28 14:18:30 +0300

    Update app.js
    add score

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let squares = Array.from(document.querySelector(".grid div"));
+  // console.table(squares);
   const scoreDisplay = document.querySelector("#score");
   const startBtn = document.querySelector("#start-button");
   const width = 10;
-  console.log(squares[0]);
+  let nextRandom = ;
+  let timerId = 0;
+  let score = 0;
+  const colors = ["orange", "red", "purple", "green", "blue"];
+
+  //The Tetrominoes
+  
+  
+  
 });
