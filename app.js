cc163cb3bb7e650d4564818fdb46f0c35f94ced7
catborn <itnoz@yandex.ru>
2024-08-28 22:36:58 +0300

    change colors figures

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let squares = Array.from(document.querySelectorAll(".grid div"));
-  // console.table(squares);
+  console.table(squares);
   const scoreDisplay = document.querySelector("#score");
   const startBtn = document.querySelector("#start-button");
   const width = 10;
   let nextRandom = 0;
   let timerId = 0;
   let score = 0;
-  const colors = ["orange", "red", "purple", "green", "blue"];
+  const colors = [
+    "lightsalmon",
+    "palevioletred",
+    "violet",
+    "greenyellow",
+    "lightskyblue",
+  ];
 
   //The Tetrominoes
   //0xx
