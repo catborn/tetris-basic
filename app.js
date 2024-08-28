78f4bd7f62d8ea528bfd584ef73e4a8c0e47321c
catborn <itnoz@yandex.ru>
2024-08-28 22:20:47 +0300

    add score

--- app.js
+++ app.js
@@ -232,3 +232,22 @@ startBtn.addEventListener('click', () => {
   }
 })
 
+//add score
+function addScore() {
+  for (let i = 0; i < 199; i += width) {
+    const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+    if(row.every(index => squares[index].classList.contains('taken'))) {
+      score += 10
+      scoreDisplay.innerHTML = score
+      row.forEach(index => {
+        squares[index].classList.remove('taken')
+        squares[index].classList.remove('tetromino')
+        squares[index].style.backgroundColor = ''  
+      })
+      const squareRemoved = squares.splice(i, width)
+      squares = squaresRemoved.concat(squares)
+      squares.forEach(cell => grid.appendChild(cell))
+    }
+  }
+}
