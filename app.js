9c4bbf8ed82b57794456a8a807231f6296efb1b7
catborn <158094798+catborn@users.noreply.github.com>
2024-09-02 12:15:17 +0300

    Update app.js
    explain first rotation

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 
   //The Tetrominoes
+  //first rotation [1, width + 1, width * 2 + 1, 2],
   //0xx
   //0x0
   //0x0
@@ -26,28 +27,36 @@ document.addEventListener("DOMContentLoaded", () => {
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2],
   ];
-
+  //xx0
+  //0xx
+  //000
   const zTetromino = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
   ];
-
+  //0x0
+  //xxx
+  //000
   const tTetromino = [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1],
   ];
-
+  //xx
+  //xx
   const oTetromino = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
   ];
-
+  //0000
+  //xxxx
+  //0000
+  //0000
   const iTetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
